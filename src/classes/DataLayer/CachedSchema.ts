9a2fc2cd13d5091f6c6886584610f5dc56285b55
Ed Nutting <github@ednutting.com>
2020-10-06 11:22:45 +0100

    feat: Bump schema version to 4 to trigger cache db upgrade

--- src/classes/DataLayer/CachedSchema.ts
+++ src/classes/DataLayer/CachedSchema.ts
@@ -13,7 +13,7 @@ export type Indexes<T> = { [K in PromisedKeys<T>]: "id" };
 //            be increased.
 
 // Decimal places are not allowed - only positive integers!
-export const SchemaVersion: number = 3;
+export const SchemaVersion: number = 4;
 
 export interface CachableDBSchema extends IDB.DBSchema {
     [s: string]: DBSchemaValue;
